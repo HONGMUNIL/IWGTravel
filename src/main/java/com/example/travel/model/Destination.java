32803cf6947dedf4e4109eeadf4066fb1268bf25
Hong <bear4564@naver.com>
2024-08-19 19:44:37 +0900

    완성

--- src/main/java/com/example/travel/model/Destination.java
+++ src/main/java/com/example/travel/model/Destination.java
@@ -10,7 +10,7 @@ public class Destination {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    public Long id;
     public String name;
     public String description;
     public String location;
@@ -21,6 +21,30 @@ public class Destination {
     public Destination(String name, String description, String location) {
         this.name = name;
         this.description = description;
+        this.location = location;
+    }
+     // Getters and Setters
+     public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
         this.location = location;
     }
 
