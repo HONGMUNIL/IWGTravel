2557c47312f34d6fc14dde1b7fcc910252f8032e
HONGMUNIL <bear4564@naver.com>
2024-09-03 19:22:11 +0900

    즐겨찾기기능 소스추가해야함

---
+++ src/main/java/com/example/travel/entity/User.java
@@ -0,0 +1,52 @@
+package main.java.com.example.travel.entity;
+
+import jakarta.persistence.*;
+import java.util.Set;
+
+
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private Set<Favorite> favorites;
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Favorite> getFavorites() {
+        return favorites;
+    }
+
+    public void setFavorites(Set<Favorite> favorites) {
+        this.favorites = favorites;
+    }
+}
