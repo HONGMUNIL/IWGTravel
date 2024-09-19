6720fab0d8b27e74e857c7bbc959b37c32c427e5
Hong <bear4564@naver.com>
2024-09-19 23:26:05 +0900

    commita

--- src/main/java/com/example/travel/entity/Destination.java
+++ src/main/java/com/example/travel/entity/Destination.java
@@ -1,9 +1,12 @@
-package com.example.travel.model;
+package com.example.travel.entity
+;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Destination {
